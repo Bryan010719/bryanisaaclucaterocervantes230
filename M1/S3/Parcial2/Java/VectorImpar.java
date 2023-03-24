2c2aef3e6b19be02a85ff75517c6377878e13a1e
Bryan Isaac Lucatero Cervantes <126211404+Bryan010719@users.noreply.github.com>
2023-03-23 19:06:22 -0600

    Add files via upload

---
+++ M1/S3/Parcial2/Java/VectorImpar.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class VectorImparPar
+{
+    public static void main(String[] args)
+    {
+        int[] listaNumeros = {0, -9, 4, -1, 3, 7};
+        for(int i = 0; i < listaNumeros.length; i++)
+        {
+            System.out.print(" "+listaNumeros[i]);
+        }
+
+
+
+    }
+}
+
+
