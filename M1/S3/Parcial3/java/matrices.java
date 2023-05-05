c5955a688ed410233162188248017ea8b4935300
Bryan Isaac Lucatero Cervantes <126211404+Bryan010719@users.noreply.github.com>
2023-05-05 10:06:13 -0600

    Add files via upload

---
+++ M1/S3/Parcial3/java/matrices.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class numalcuadrado
+{
+    public static void main(String[] args)
+    {
+
+        int[][] A = {{5, 1, 0}, {3, 2, 0}, {5, 3, 0}};
+        for (int f = 0; f < A.length; f++)
+        {
+            for (int c = 0; c < A[f].length; c++)
+            {
+                {
+                    System.out.print(A[f][c] + " ");
+                }
+
+
+
+
+            }
+            System.out.println();
+
+
+
+
+        }
+
+
+
+
+    }
+}
+
+
+
+
